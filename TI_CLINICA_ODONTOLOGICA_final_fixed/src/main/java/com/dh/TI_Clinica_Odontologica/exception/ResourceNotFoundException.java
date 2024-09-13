3e146f5af9278322f37c1d6f3d73f4d9c5656e5a
dahbit <davide.acv@gmail.com>
2024-09-13 02:30:57 -0600

    Delete TI_CLINICA_ODONTOLOGICA_final_fixed directory

--- TI_CLINICA_ODONTOLOGICA_final_fixed/src/main/java/com/dh/TI_Clinica_Odontologica/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.dh.TI_Clinica_Odontologica.exception;
-
-//creamos nuestra propia excepción
-public class ResourceNotFoundException extends RuntimeException {
-    //creo un constructor
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- TI_CLINICA_ODONTOLOGICA_final_fixed/src/main/java/com/dh/TI_Clinica_Odontologica/repository/IPacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dh.TI_Clinica_Odontologica.repository;
-
-import com.dh.TI_Clinica_Odontologica.entity.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IPacienteRepository extends JpaRepository<Paciente, Long> {
-}
